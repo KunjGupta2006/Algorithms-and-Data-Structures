5cf6801681920e9724400158f7fac81d17ad502b
Kunj Gupta <kunjg536@gmail.com>
2026-07-21 15:12:04 +0530

    Time: 3 ms (67.91%) | Memory: 26.8 MB (15.21%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.cpp
+++ 680-valid-palindrome-ii/valid-palindrome-ii.cpp
@@ -12,14 +12,11 @@ public:
     bool validPalindrome(string s) {
         int l=0;
         int h=s.length()-1;
-        int flag=0;
         while(l<=h){
-            if(s[l]!=s[h] && flag==0 ){
-                if( check(s,l+1,h) ) { flag=1; l++; continue; }
-                else if( check(s,l,h-1)) { flag=1; h--; continue; }
+            if(s[l]!=s[h] ){
+                if( check(s,l+1,h) ) { return true; }
+                else if( check(s,l,h-1)) { return true; }
                 else return false;
-            }else if( s[l]!=s[h] && flag ){
-                return false; 
             }
             l++;h--;
         }
